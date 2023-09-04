2ee3f16249db3464010dde663cbdd3f7c68a6a98
Jung JooWon <jooowon.dev@gmail.com>
2023-09-04 10:58:43 +0900

    feat. init commit

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import NavBar from "../../components/NavBar";
+import Head from "next/head";
+import Seo from "../../components/Seo";
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <Seo title="About" />
+      </Head>
+      <h1>About</h1>
+    </div>
+  );
+}
